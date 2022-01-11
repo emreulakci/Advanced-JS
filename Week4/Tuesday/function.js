2906f851a17d232f96487ed51977dd3e4a11c8c5
emreulakci <72080698+emreulakci@users.noreply.github.com>
2022-01-11 22:59:10 +0100

    Update function.js

--- Week4/Tuesday/function.js
+++ Week4/Tuesday/function.js
@@ -1,1 +1,10 @@
-
+// Maasi belirli bir degerin üzerinde olanlari bulmak icin fonksiyon
+function findOverSalaryList(pFamilyList, pSalary) {
+    let overOverSalaryList = pFamilyList.filter((salaries) => salaries.salary > pSalary);
+    return overOverSalaryList;
+};
+// Alfabetik siralama
+function sortAlphabeticalList(pFirstList) {
+    let alphabeticalList = pFirstList.sort();
+    return alphabeticalList;
+};
