40c21e7caff887e7f9e1df0f3b97b224da4c0cd1
emreulakci <72080698+emreulakci@users.noreply.github.com>
2022-01-20 23:58:38 +0100

    app.js

---
+++ Week5/Thursday/app.js
@@ -0,0 +1,7 @@
+et batteryLevel = 0;
+const batteryLevelOne = document.querySelector("#batteryLevelOne");
+const batteryLevelTwo = document.querySelector("#batteryLevelTwo");
+const batteryLevelThree = document.querySelector("#batteryLevelThree");
+const batteryText = document.querySelector("#batteryShow");
+document.querySelector("#plus").addEventListener("click", fullBattery);
+document.querySelector("#minus").addEventListener("click", emptyBattery);
